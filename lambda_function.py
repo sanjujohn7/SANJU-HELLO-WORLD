edcbfbc74fab90bfec9456b9753ef5b9446e268d
Sanju John <sanju.john@edstem.com>
2022-04-09 06:33:48 +0000

    commit

--- lambda_function.py
+++ lambda_function.py
@@ -18,16 +18,14 @@ def wish_hello_handler(event, context):
         temp_file = "/tmp/hello_local.txt"
         s3.Bucket(BUCKET_NAME).download_file(KEY, temp_file )
         file = open(temp_file, "r")
-        print(file.read())
+        return {
+        'statusCode': 200,
+        'body': file.read()
+        }
+        
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             logging.error("The object does not exist.")
         else:
             raise
     
-    
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': file.read()
-    }
