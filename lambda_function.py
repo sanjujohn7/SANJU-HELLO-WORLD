9b6819b565da6100faede4a6f78dd9e23ee3ed7e
Sanju John <sanju.john@edstem.com>
2022-04-09 05:35:41 +0000

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+import botocore
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+BUCKET_NAME = 'dev-days-test' # replace with your bucket name
+KEY = 'hello.txt' # replace with your object key
+
+s3 = boto3.resource('s3')
+
+def wish_hello_handler(event, context):
+    
+    logging.info(event)
+    
+    try:
+        s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/hello_local.txt')
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            logging.error("The object does not exist.")
+        else:
+            raise
+    
+    
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
